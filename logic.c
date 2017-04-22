cca8e90728860a44aa8b4e59af989ff9780805cc
Iain Hill <iainardernhill@gmail.com>
2017-04-22 17:14:28 +0200

    licence

--- logic.c
+++ logic.c
@@ -5,28 +5,32 @@
  Version     : 1
  Copyright   : GPL
  
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
  Description : naughts and crosses / tic tac toe
+
+    Copyright (C) 2017 Iain Hill. 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  ============================================================================
  */
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
 #include "ticTacToe.h"
 
-// The size of the playing board, the 'hash'.
+/* The size of the playing board, the 'hash'. */
 #define MATRIX	9
 #define M_SQRT	3
 
@@ -267,7 +271,7 @@ int headsOrTails()
 	for(;;)
 	{
 		int c;
-		int choice;
+		int choice = 'h';
 		drawGrid(RESET);
 		sysOut(4, 0);
 		while ((c = getchar()) != '\n')
